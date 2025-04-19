d7b809c25409d84321d8d806667d1df9f013c25f
Huynh Huy Hoang <huynhhuyhoang05022005@gmail.com>
2025-04-18 22:17:20 -0400

    fourth  commit

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 8080;
 const studentName = "Huynh Huy Hoang";
 const studentId = "151569233";
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(expressLayouts);
@@ -36,7 +36,6 @@ app.set("view engine", "ejs");
 app.use(expressLayouts); 
 app.set("views", path.join(__dirname, "views"));
 app.set('layout', 'layouts/main');
-
 
 app.use(clientSessions({
     cookieName: "session", 
@@ -101,8 +100,8 @@ app.get("/", (req, res) => {
 });
 
 app.get('/main', (req, res) => {
-    res.render('home', { layout: 'layouts/main' });
-  });
+    res.redirect('/shop');
+});
 
 app.get("/about", (req, res) => {
     res.render("about", { title: "About Huynh Huy Hoang" });
